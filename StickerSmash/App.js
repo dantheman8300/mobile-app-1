a9977677c3eb0f81ab56245ab18aea6290e518ab
dantheman8300 <daniel.7039@hotmail.com>
2024-02-15 12:08:06 -0800

    platform specifics

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,7 +1,7 @@
 import { useState, useRef } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
@@ -13,6 +13,9 @@ import EmojiSticker from './components/EmojiSticker';
 
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+
+import domtoimage from 'dom-to-image';
+
 
 
 import * as ImagePicker from 'expo-image-picker';
@@ -53,20 +56,37 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Saved!");
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+  
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
+  
 
   const onAddSticker = () => {
     setIsModalVisible(true);
